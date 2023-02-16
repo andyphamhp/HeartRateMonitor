91368058df0e358f341d11aa6cf86c116aaa5e2c
Andy Pham <andyphamhp9x@gmail.com>
2023-02-16 11:41:03 +0700

    Initial Commit

---
+++ HeartRateMonitor/HeartRateMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateMonitorApp.swift
+//  HeartRateMonitor
+//
+//  Created by Andy Pham on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
